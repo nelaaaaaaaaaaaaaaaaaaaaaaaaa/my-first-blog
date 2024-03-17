f7c750efffbdc3beda3207176b98a98099722162
Nela <ty@example.com>
2024-03-17 12:19:41 +0100

    My Django Girls app, first commit

---
+++ test.py
@@ -0,0 +1,10 @@
+def ahoj(meno):
+    print ("Ahoj " + meno + "!")
+    
+dievcata = ["Katka", "Kika", "Zuza", "Nela", "Ty"]
+for meno in dievcata:
+    ahoj(meno)
+    print("Dalsie dievca")
+    
+for i in range(1, 6):
+    print(i)
